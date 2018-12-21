2062d1123c9e8b423cd3774f6d7f8d8f1ee8c00d
Hendrik Ranocha <h.ranocha@tu-bs.de>
2018-12-21 11:42:57 +0100

    improve performance for periodic boundaries

--- include/2ndOrder.h
+++ include/2ndOrder.h
@@ -1,9 +1,53 @@
 // This project is licensed under the terms of the Creative Commons CC BY-NC-ND 4.0 license.
 
-// Contains the coefficients of second order derivative operator
+/* Contains the coefficients of the second order diagonal norm derivative operator of
+@article{mattsson2004summation,
+  title={Summation by parts operators for finite difference approximations of
+         second derivatives},
+  author={Mattsson, Ken and Nordstr{\"o}m, Jan},
+  journal={Journal of Computational Physics},
+  volume={199},
+  number={2},
+  pages={503--540},
+  year={2004},
+  publisher={Elsevier},
+  doi={10.1016/j.jcp.2004.03.001}
+}
+*/
 
+#ifdef USE_PERIODIC
 //------------------------------------------------------------------------------
-// Derivative Operator
+// Periodic Derivative Operator
+//------------------------------------------------------------------------------
+#define ORDER 2
+#define NUM_BOUNDS 0
+#define STENCIL_WIDTH 3
+
+/*
+Coefficients of the first derivative operator.
+
+For each row, the central coefficient corresponds to the nodes at which the
+derivative is computed.
+*/
+REAL constant SBP_diff[2*NUM_BOUNDS+1][STENCIL_WIDTH] = {
+  // central coefficients
+  {-1.0/2.0, 0.0, 1.0/2.0},
+};
+
+
+/*
+Coefficients of the inverse mass/norm matrix.
+
+The central coefficient corresponds to the inner nodes of the grid and is 1.
+*/
+REAL constant M_INV[2*NUM_BOUNDS+1] = {
+  // central coefficients
+  1.0,
+};
+
+#else
+//------------------------------------------------------------------------------
+// Non-Periodic Derivative Operator
 //------------------------------------------------------------------------------
 
 #define ORDER 2
@@ -39,3 +83,5 @@ REAL constant M_INV[2*NUM_BOUNDS+1] = {
   // right boundary coefficients
   2.0
 };
+
+#endif // USE_PERIODIC
