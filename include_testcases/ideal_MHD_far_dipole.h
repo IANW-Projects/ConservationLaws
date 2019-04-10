4f4d34fc4d67462af7b62823338ea9acb3de0bcb
Hendrik Ranocha <h.ranocha@tu-bs.de>
2019-04-10 14:29:46 +0200

    rebase on master

--- include_testcases/ideal_MHD_far_dipole.h
+++ include_testcases/ideal_MHD_far_dipole.h
@@ -25,7 +25,7 @@ inline REAL4 b_analytical(uint ix, uint iy, uint iz, REAL time) {
 	return (REAL4) {(REAL)(0), (REAL)(0), (REAL)(0), (REAL)(0)};
 }
 
-// Magnetic Dipole definition out of 
+// Magnetic Dipole definition out of
 // Course of Theoretical Physics: The Classical Theory of Fields
 // L.D. Landau and E. M. Lifschitz
 // (1951)
@@ -40,13 +40,11 @@ inline REAL4 b_dipole(REAL4 X, REAL4 M) {
 /* initial values for p, rho, B and the velocity */
 
 inline REAL p_initial(uint ix, uint iy, uint iz, REAL time){
-	
 
 	return (REAL)(1.0);
 }
 
 inline REAL rho_initial(uint ix, uint iy, uint iz, REAL time){
-
 
 	return (REAL)(1.0);
 }
@@ -57,19 +55,19 @@ inline REAL4 b_initial(uint ix, uint iy, uint iz, REAL time){
 	REAL x = XMIN + ix * DX - X_0.x;
 	REAL y = YMIN + iy * DY - X_0.y;
 	REAL z = ZMIN + iz * DZ - X_0.z;
-	
+
 	return b_dipole((REAL4){x, y, z, 0}, M);
 }
 
 inline REAL4 u_initial(uint ix, uint iy, uint iz, REAL time){
-	
+
 	return (REAL4){0, 0, 0, 0} ;
 }
 
 
 /*
 Boundary condition of the magnetic field.
-The absolute magnetic field of the right boundary in x-direction oscillates. 
+The absolute magnetic field of the right boundary in x-direction oscillates.
 We would like to see the waves which propagate to the left.
 */
 inline REAL4 b_boundary(uint ix, uint iy, uint iz, REAL time) {
@@ -78,12 +76,14 @@ inline REAL4 b_boundary(uint ix, uint iy, uint iz, REAL time) {
 	REAL z = ZMIN + iz * DZ;
 	REAL amplitude;
 	REAL4 init_b = b_initial(ix, iy, iz, (REAL)0.0);
-	if (x > 0.9)		
+	if (x > 0.9) {
 		amplitude = 1 + A0 * sin(omega*time) * (x - 0.9) * 10;
-	else
+  }
+	else {
 		amplitude = 1;
+  }
 
-        return init_b * amplitude;
+  return init_b * amplitude;
 }
 
 /*
@@ -91,14 +91,15 @@ Boundary condition of the velocity field.
 */
 inline REAL4 u_boundary(uint ix, uint iy, uint iz, REAL time) {
 
-
-        return u_initial(ix, iy, iz, time);
+  return u_initial(ix, iy, iz, time);
 }
 
 inline REAL rho_boundary(uint ix, uint iy, uint iz, REAL time) {
-	return 1;
+
+	return rho_initial(ix, iy, iz, time);
 }
 
 inline REAL p_boundary(uint ix, uint iy, uint iz, REAL time){
-	return 1;
+
+	return p_initial(ix, iy, iz, time);
 }
